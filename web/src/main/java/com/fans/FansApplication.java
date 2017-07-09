4d0589240172dcf964f2195c82c0a8b1d9281645
yxxu <94478826@qq.com>
2017-07-09 22:23:34 +0800

    fans init

---
+++ web/src/main/java/com/fans/FansApplication.java
@@ -0,0 +1,12 @@
+package com.fans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FansApplication.class, args);
+	}
+}
