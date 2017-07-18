a9e5bb1757cc0f308468b7c8ed5053236829a708
yxxu <94478826@qq.com>
2017-07-18 23:40:23 +0800

    add

--- service/src/main/java/com/fans/service/FansUserDetailsService.java
+++ service/src/main/java/com/fans/service/FansUserDetailsService.java
@@ -3,13 +3,16 @@ package com.fans.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.fans.domain.user.User;
+@Service
 public class FansUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
 		// TODO Auto-generated method stub
-		return null;
+		return new User();
 	}
 
 }
