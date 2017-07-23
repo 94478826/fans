53f120443cda586c78a87967e153bc493a6394be
yxxu <94478826@qq.com>
2017-07-23 23:26:31 +0800

    add

--- web/src/main/java/com/fans/FansApplication.java
+++ web/src/main/java/com/fans/FansApplication.java
@@ -2,10 +2,17 @@ package com.fans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FansApplication {
+public class FansApplication extends SpringBootServletInitializer {
+    
+   @Override
+   protected SpringApplicationBuilder configure(
+           SpringApplicationBuilder application) {
+       return application.sources(FansApplication.class);
+   }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FansApplication.class, args);
